3bafe779392a85b65d7f6821fd67d15b800a9e1a
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2021-06-14 12:38:19 -0600

    Update body.dart

--- lib/screens/Dashboard/body.dart
+++ lib/screens/Dashboard/body.dart
@@ -1,107 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/constants.dart';
-import 'package:flutter/material.dart';
 
-class Dashboard_Screen extends StatefulWidget {
+class Dashboard extends StatefulWidget {
   @override
-  _DashboardScreenState createState() => _DashboardScreenState();
+  _DashboardState createState() => _DashboardState();
 }
 
-class _DashboardScreenState extends State<Dashboard_Screen>
-    with TickerProviderStateMixin {
-
-  List<Widget> listViews = <Widget>[];
-  final ScrollController scrollController = ScrollController();
-  double topBarOpacity = 0.0;
-
-  @override
-  void initState() {
-    addAllListData();
-
-    scrollController.addListener(() {
-      if (scrollController.offset >= 24) {
-        if (topBarOpacity != 1.0) {
-          setState(() {
-            topBarOpacity = 1.0;
-          });
-        }
-      } else if (scrollController.offset <= 24 &&
-          scrollController.offset >= 0) {
-        if (topBarOpacity != scrollController.offset / 24) {
-          setState(() {
-            topBarOpacity = scrollController.offset / 24;
-          });
-        }
-      } else if (scrollController.offset <= 0) {
-        if (topBarOpacity != 0.0) {
-          setState(() {
-            topBarOpacity = 0.0;
-          });
-        }
-      }
-    });
-    super.initState();
-  }
-
-  void addAllListData() {
-    const int count = 9;
-
-  }
-
-  Future<bool> getData() async {
-    await Future<dynamic>.delayed(const Duration(milliseconds: 50));
-    return true;
-  }
-
+class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: quarternary,
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ARC"),
+        elevation: .1
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(3.0),
           children: <Widget>[
-            getMainListViewUI(),
-            getAppBarUI(),
-            SizedBox(
-              height: MediaQuery.of(context).padding.bottom,
-            )
+            ActivitiesItem ("Ordbog", Icons.book),
+            ActivitiesItem ("Alphabet", Icons.alarm),
+            ActivitiesItem ("Alphabet", Icons.alarm),
+            ActivitiesItem("Alphabet", Icons.alarm),
+            ActivitiesItem("Alphabet", Icons.alarm),
+            ActivitiesItem("Alphabet", Icons.alarm)
           ],
         ),
       ),
-    );
+      );
   }
+}
 
-  Widget getMainListViewUI() {
-    return FutureBuilder<bool>(
-      future: getData(),
-      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-        if (!snapshot.hasData) {
-          return const SizedBox();
-        } else {
-          return ListView.builder(
-            controller: scrollController,
-            padding: EdgeInsets.only(
-              top: AppBar().preferredSize.height +
-                  MediaQuery.of(context).padding.top +
-                  24,
-              bottom: 62 + MediaQuery.of(context).padding.bottom,
-            ),
-            itemCount: listViews.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (BuildContext context, int index) {
-              return listViews[index];
-            },
-          );
-        }
-      },
-    );
-  }
-
-  Widget getAppBarUI() {
-    return Column(
-      children: <Widget>[
-      ],
-    );
-  }
+Card ActivitiesItem (String title, IconData icon) {
+  return Card(
+      elevation: 1.0,
+      margin: new EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+        child: new InkWell(
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.down,
+            children: <Widget>[
+              SizedBox(height: 50.0),
+              Center(
+                  child: Icon(
+                    icon,
+                    size: 40.0,
+                    color: Colors.black,
+                  )),
+              SizedBox(height: 20.0),
+              new Center(
+                child: new Text(title,
+                    style:
+                    new TextStyle(fontSize: 18.0, color: Colors.black)),
+              )
+            ],
+          ),
+        ),
+      ));
 }
