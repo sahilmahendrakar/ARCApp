525f19f3ce6fa51e65c0bf603b2cddc2b20d55db
sahilmahendrakar <sahilm020@gmail.com>
2021-06-24 13:35:41 -0400

    Added sign out button to settings

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,23 +4,34 @@ import 'package:arc_app/screens/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentIndex = 0;
+  final List<Widget> children = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Text("Home"),
-          ElevatedButton(onPressed: () {
-              Navigator.pushNamed(
-                context, SettingsScreen.routeName);
-            }, child: Text("Settings")),
-          ElevatedButton(onPressed: () {
-            context.read<AuthenticationService>().signOut();
-          },
-          child: Text("Sign Out"),)
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, SettingsScreen.routeName);
+              },
+              child: Text("Settings")),
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthenticationService>().signOut();
+            },
+            child: Text("Sign Out"),
+          )
         ],
       )),
     );
--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,6 +1,10 @@
+import 'package:arc_app/auth/authentication_service.dart';
+import 'package:arc_app/constants.dart';
+import 'package:arc_app/size_config.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -35,56 +39,83 @@ class _SettingsScreenState extends State<SettingsScreen> {
         appBar: AppBar(
           title: Text("Settings"),
         ),
-        body: SettingsList(
-          sections: [
-            SettingsSection(
-              title: 'Profile',
-              tiles: [
-                SettingsTile(
-                  title: 'Username',
-                  subtitle: user.displayName,
-                  onPressed: (context) {},
-                ),
-                SettingsTile(
-                  title: 'Study ID',
-                  subtitle: studyId,
-                  onPressed: (context) {},
-                ),
-                SettingsTile(
-                  title: 'Password',
-                  subtitle: '*********',
-                  onPressed: (context) {},
-                )
-              ],
+        body: Column(
+          children: [
+            Expanded(
+              child: SettingsList(
+                sections: [
+                  SettingsSection(
+                    title: 'Profile',
+                    tiles: [
+                      SettingsTile(
+                        title: 'Username',
+                        subtitle: user.displayName,
+                        onPressed: (context) {},
+                      ),
+                      SettingsTile(
+                        title: 'Study ID',
+                        subtitle: studyId,
+                        onPressed: (context) {},
+                      ),
+                      SettingsTile(
+                        title: 'Password',
+                        subtitle: '*********',
+                        onPressed: (context) {},
+                      )
+                    ],
+                  ),
+                  SettingsSection(
+                    title: 'Notifications',
+                    tiles: [
+                      SettingsTile.switchTile(
+                          title: 'Do Not Disturb',
+                          switchValue: doNotDisturb,
+                          onToggle: (value) {
+                            setState(() {
+                              doNotDisturb = value;
+                            });
+                          }),
+                      SettingsTile(title: 'Preset Times'),
+                      SettingsTile(title: 'Notification Type')
+                    ],
+                  ),
+                  SettingsSection(
+                    title: 'Contacts',
+                    tiles: [
+                      SettingsTile(
+                        title: 'Emergency Contact',
+                        subtitle: '123-456-7890',
+                      ),
+                      SettingsTile(
+                        title: 'Trusted Support',
+                        subtitle: '123-456-7890',
+                      ),
+                    ],
+                  ),
+                  CustomSection(
+                      child: Padding(
+                        padding: EdgeInsets.all(getProportionateScreenHeight(20)),
+                        child: Center(
+                    child: ElevatedButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: primary,
+                            primary: Colors.white, shape: StadiumBorder()),
+                        child: Padding(padding: EdgeInsets.fromLTRB(getProportionateScreenWidth(20), getProportionateScreenHeight(10), getProportionateScreenHeight(20), getProportionateScreenWidth(10)),
+                        child: Text("Sign Out")),
+                        onPressed: () {
+                          context
+                              .read<AuthenticationService>()
+                              .signOut()
+                              .then((value) {
+                            Navigator.pop(context);
+                          });
+                        },
+                    ),
+                  ),
+                      ))
+                ],
+              ),
             ),
-            SettingsSection(
-              title: 'Notifications',
-              tiles: [
-                SettingsTile.switchTile(
-                    title: 'Do Not Disturb',
-                    switchValue: doNotDisturb,
-                    onToggle: (value) {
-                      setState(() {
-                        doNotDisturb = value;
-                      });
-                    }),
-                SettingsTile(title: 'Preset Times'),
-                SettingsTile(title: 'Notification Type')
-              ],
-            ),
-            SettingsSection(
-              title: 'Contacts',
-              tiles: [
-                SettingsTile(
-                  title: 'Emergency Contact',
-                  subtitle: '123-456-7890',
-                ),
-                SettingsTile(
-                  title: 'Trusted Support',
-                  subtitle: '123-456-7890',
-                ),
-              ],
-            )
           ],
         ));
   }
