a4d930511ad6bb94d7ffea35f9f3ec92cbf872b7
apumaparna <aparnakumargs@gmail.com>
2021-07-10 23:02:47 -0400

    Details with fake data added

--- lib/screens/Summary/summary_body.dart
+++ lib/screens/Summary/summary_body.dart
@@ -37,17 +37,58 @@ class _SummaryState extends State<SummaryBody> {
               ),
             ),
           ),
-          summaryItem(context, secondary, "Mood"),
-          summaryItem(context, darkestBlue, "Stress"),
-          summaryItem(context, secondary, "Heart"),
-          summaryItem(context, darkestBlue, "Sleep")
+          summaryItem(context, darkestBlue, "Mood", "6.5/10", "Good", "1.5", 1,
+              tertiary),
+          summaryItem(context, secondary, "Stress", "52 units", "Okay", "18", 1,
+              brightPink),
+          summaryItem(context, darkestBlue, "Heart", "79 bpm", "Good", "0", 0,
+              brightYellow),
+          summaryItem(context, secondary, "Sleep", "5h 41m", "Bad", "2h 31m",
+              -1, brightPink)
         ],
       ),
     );
   }
 }
 
-GestureDetector summaryItem(BuildContext context, Color color, String title) {
+GestureDetector summaryItem(
+    BuildContext context,
+    Color color,
+    String title,
+    String value,
+    String status,
+    String change,
+    int direction,
+    Color arrowColor) {
+  // Set the status color
+  Color statusColor;
+  if (status == "Good") {
+    statusColor = tertiary;
+  } else if (status == "Okay") {
+    statusColor = brightYellow;
+  } else {
+    statusColor = brightPink;
+  }
+
+  // Set the Details Text style
+  TextStyle detailsStyle =
+      TextStyle(color: pureWhite, fontSize: 20, fontWeight: FontWeight.bold);
+
+  // Set the arrow
+  Icon arrow;
+
+  if (direction == 1) {
+    arrow = Icon(Icons.arrow_upward_rounded,
+        color: arrowColor, size: getProportionateScreenHeight(30));
+  } else if (direction == -1) {
+    arrow = Icon(Icons.arrow_downward_rounded,
+        color: arrowColor, size: getProportionateScreenHeight(30));
+  } else {
+    arrow = Icon(Icons.horizontal_rule_rounded,
+        color: arrowColor, size: getProportionateScreenWidth(25));
+  }
+
+  // Create the page
   return GestureDetector(
       onTap: () {
         //TODO: Change the destination page
@@ -72,14 +113,61 @@ GestureDetector summaryItem(BuildContext context, Color color, String title) {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text(
-                        title,
-                        style: TextStyle(
-                          color: pureWhite,
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(
+                            0,
+                            getProportionateScreenHeight(10),
+                            0,
+                            getProportionateScreenHeight(10)),
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                            color: pureWhite,
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
+                      Table(
+                          defaultVerticalAlignment:
+                              TableCellVerticalAlignment.middle,
+                          children: <TableRow>[
+                            TableRow(
+                              children: <Widget>[
+                                Center(
+                                  child: Text(
+                                    value,
+                                    style: detailsStyle,
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 0,
+                                          getProportionateScreenWidth(5), 0),
+                                      child: Container(
+                                        width: 18,
+                                        height: 18,
+                                        decoration: new BoxDecoration(
+                                          color: statusColor,
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(status, style: detailsStyle)
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    arrow,
+                                    Text(change, style: detailsStyle)
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ]),
                     ]))),
       ));
 }
